1fc2ab538e3aa15c0c3fa487195aeb4dbf833b3e
Ronald Macario <ronaldmacario13@gmail.com>
2023-08-09 00:06:15 -0300

    feat: iniciando a criacao do app

---
+++ src/screen/NuBank/NuBank.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Container } from "./NuBank.styles";
+import { Header } from "../../components/Header/Header";
+
+export function Nubank() {
+    return (
+        <Container>
+            <Header />
+        </Container>
+    )
+}
